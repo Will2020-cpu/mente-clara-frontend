c9b164ca4b9b7e5d57df8e728ea369155e15ba6a
Willian Camacho <61927270+Will2020-cpu@users.noreply.github.com>
2021-12-05 03:12:16 -0500

    update enviorement

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   apiUrl:'https://quiet-sea-92909.herokuapp.com'
 };
 
