b649657502ccd60db47c99e5b9ba4c31483feab2
madara <wcamacho221@gmail.com>
2021-12-05 03:22:46 -0500

    funcionando

--- src/app/services/articulo.service.ts
+++ src/app/services/articulo.service.ts
@@ -10,21 +10,22 @@ import { ArticulosModel } from '../models/articulo.models'
 export class ArticuloService {
 
   constructor(private http:HttpClient) { }
+  public apiUrl:string = 'https://quiet-sea-92909.herokuapp.com'
 
   public getArticulos():Promise<any>{
-    return this.http.get(`${environment.apiUrl}/api/articulos`).toPromise();
+    return this.http.get(`${this.apiUrl}/api/articulos`).toPromise();
   }
 
   public getArticuloById(id:number):Promise<any>{
-    return this.http.get(`${environment.apiUrl}/api/articulos/get/${id}`).toPromise();
+    return this.http.get(`${this.apiUrl}/api/articulos/get/${id}`).toPromise();
   }
 
   public agregarArticulos(Articulo: ArticulosModel):Promise<any>{
-    const url = `${environment.apiUrl}/api/articulos/add`
+    const url = `${this.apiUrl}/api/articulos/add`
     return this.http.post(url,Articulo).toPromise();
   }
 
   public eliminarArticulo(id:number):Promise<any>{
-    return this.http.delete(`${environment.apiUrl}/api/articulos/delete/${id}`).toPromise();
+    return this.http.delete(`${this.apiUrl}/api/articulos/delete/${id}`).toPromise();
   }
 }
